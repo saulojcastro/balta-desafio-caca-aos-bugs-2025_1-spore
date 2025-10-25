6d69559f81d3f2ba174513d6b7eed817b6482290
Saulo Castro <saulo.castro@gmail.com>
2025-10-25 19:22:04 -0300

    Adicionado a connectionString no DBcontext.

--- src/Data/AppDbContext.cs
+++ src/Data/AppDbContext.cs
@@ -11,5 +11,5 @@ public class AppDbContext : DbContext
     public DbSet<OrderLine> OrderLines { get; set; } = null!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlite();
+        => optionsBuilder.UseSqlite("Data Source=app.db");
 }
