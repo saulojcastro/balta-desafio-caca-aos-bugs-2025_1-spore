0eab93707a4a6cf9f5ee80fc08457dc45147e7bf
Saulo Castro <saulo.castro@gmail.com>
2025-10-26 12:11:10 -0300

    Refatorado o preenchimento da connectionString,

--- src/Configs/DbContextConfiguration.cs
+++ src/Configs/DbContextConfiguration.cs
@@ -7,11 +7,7 @@ namespace BugStore.Configs
     {
         public static WebApplicationBuilder AddDbContextConfiguration(this WebApplicationBuilder builder)
         {
-            var _connectionString = string.Empty;
-
-            if(builder.Environment.IsDevelopment())
-                _connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
+            var _connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
             builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(_connectionString));
 
             return builder;
